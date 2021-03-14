ea0ae94e3f3e3f1883062a684d4934f1111023ee
mezianefraga00 <mezianefraga00@gmail.com>
2021-03-13 23:07:28 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Code your solutions in this file
+function writeCards() {
+  const name = ["Lisa", "Kaitlin", "Jan"];
+  const result = [];
+
+  for (let i = 0; i < name.length; i++) {
+    //console.log(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+    result.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+    
+    //"Thank you, Ada, for the wonderful birthday gift!",
+    debugger;
+   
+  }
+  return result
+  }
+
+writeCards();
+
+/*const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+*/
+
+function countDown(){
+    let x = 0;
+    let n = 10;
+    do {
+        console.log(n-x);
+        x++;
+        debugger;
+    }
+    while (x<=n);
+
+    
+
+}
+countDown();
